16022fbca1792cba5303a33e73f5242cee1d523a
UshnaIrfan <ushhnairfan77@gmail.com>
2023-03-28 10:24:19 +0500

    formatting.

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,23 @@ async function bootstrap() {
        app.use(bodyParser.json({ limit: '50mb' }));
        app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-     //Swagger connection
-      const config = new DocumentBuilder()
-     .setTitle('Boiler_Plate')
-     .setDescription('boilerplate backend')
-     .setVersion('1.0')
-     .addTag('boilerplate')
-     .addBearerAuth({ in: 'header', type: 'http' })
-     .build();
-     const document = SwaggerModule.createDocument(app, config);
-     SwaggerModule.setup('api', app, document);
+      //Swagger connection
+       const config = new DocumentBuilder()
+      .setTitle('Boiler_Plate')
+      .setDescription('boilerplate backend')
+      .setVersion('1.0')
+      .addTag('boilerplate')
+      .addBearerAuth({ in: 'header', type: 'http' })
+      .build();
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('api', app, document);
 
-    // Server Connection
-     const port = process.env.PORT || 3000;
-     await app.listen(port, async () => {
-     console.log(
+     // Server Connection
+      const port = process.env.PORT || 3000;
+      await app.listen(port, async () => {
+      console.log(
       `The server is running on ${port} port: http://localhost:${port}/api`,
-    );
+        );
   });
 }
 bootstrap();
