bb4db0a2ae712ffecaaadcf6bbe1626ceba5f368
UshnaIrfan <ushhnairfan77@gmail.com>
2023-04-13 16:51:00 +0500

    intial code

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as fs from "fs";
 
 async function bootstrap() {
 
-       const app = await NestFactory.create(AppModule, { cors: {
-           credentials: true,
-           origin: [
-             'http://localhost:3000',
-             'http://localhost:3001'
-           ],
-         }, });
+     // const httpsOptions = {
+     //     key: fs.readFileSync(`${process.env.SSL_PRIVATE_KEY_FILE}`),
+     //     cert: fs.readFileSync(`${process.env.SSL_FULL_CHAIN_FILE}`),
+     // };
+     //
+     //  // const app = await NestFactory.create(AppModule, { httpsOptions });
+     //   const app = await NestFactory.create(AppModule, { httpsOptions, cors: {
+     //       credentials: true,
+     //       origin: [
+     //         `${process.env.FRONTEND_APP_URL}:3000`,
+     //         `${process.env.FRONTEND_APP_URL}:3001`
+     //       ],
+     //     }, });
+
+  // const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule, { cors: {
+      credentials: true,
+      origin: [
+        `${process.env.FRONTEND_APP_URL}:3000`,
+        `${process.env.FRONTEND_APP_URL}:3001`
+      ],
+    }, });
 
 
        app.use(bodyParser.json({ limit: '50mb' }));
