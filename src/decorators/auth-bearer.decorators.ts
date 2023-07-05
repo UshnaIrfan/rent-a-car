b3dd6539eeb52cd28599b99152a7db84260fae7d
UshnaIrfan <ushhnairfan77@gmail.com>
2023-07-05 18:52:37 +0500

    code  updated

--- src/decorators/auth-bearer.decorators.ts
+++ src/decorators/auth-bearer.decorators.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 const AuthBearer = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const { headers } = ctx.switchToHttp().getRequest();
-
     return headers.authorization.split(' ')[1];
   },
 );
